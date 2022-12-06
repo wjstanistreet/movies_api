2a1a183381ddffbcc4315bda9b97cd5b5cb9ec0e
Wills BNTA Macbook <williamstanistreet0299@gmail.com>
2022-12-06 16:14:56 +0000

    added packages and Movie POJO

---
+++ src/main/java/com/example/movies_api/models/Movie.java
@@ -0,0 +1,45 @@
+package com.example.movies_api.models;
+
+public class Movie {
+
+    private int id;
+    private String title;
+    private int rating;
+    private int duration;
+
+    public Movie(String title, int rating, int duration){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
